314dfdf721713c26266c958f07f4d506b37626f6
changxiangyu <1792286994@qq.com>
2020-03-05 22:43:45 +0800

    gitignore

--- untitled/pachong.py
+++
@@ -1,9 +0,0 @@
-#-*- encoding=UTF-8 -*-
-import requests
-from bs4 import BeautifulSoup
-
-content = requests.get('http://www.yicommunity.com/').content
-soup=BeautifulSoup(content, 'html.parser')
-
-for div in soup.find_all('div', {'class':'content'}):
-    print div.text.strip()
